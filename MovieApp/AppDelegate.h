9b30448f91333d75a60c2c36eadd3fb35d5ff5e7
mohamed samir <mmaa2012377@gmail.com>
2024-07-14 22:25:13 +0300

    Initial Commit

---
+++ MovieApp/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  MovieApp
+//
+//  Created by ios on 14/07/2024.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ MovieApp/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  MovieApp
+//
+//  Created by ios on 14/07/2024.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
